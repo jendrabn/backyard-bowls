6c2fb03d9377fde2d22af649f3b16121f6eee4eb
jendrabn <jendra455@gmail.com>
2026-01-06 07:00:00 +0700

    chore: Configure BundleAnalyzerPlugin for static reports and update build script to use `npm run`.

--- webpack.common.js
+++ webpack.common.js
@@ -65,6 +65,9 @@ module.exports = {
       ],
       overrideExtension: true,
     }),
-    new BundleAnalyzerPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+      openAnalyzer: false,
+    }),
   ],
 };
